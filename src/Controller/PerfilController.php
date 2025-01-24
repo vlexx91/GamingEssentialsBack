a0678f7127d90fc145b947de591d13de1a9e6bb6
davidmato <davidmato2005@gmail.com>
2025-01-24 16:41:59 +0100

    creacion del perfil junto a su usuario

--- src/Controller/PerfilController.php
+++ src/Controller/PerfilController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\DTO\CrearUsuarioPerfilDTO;
+use App\Entity\Perfil;
+use App\Entity\Usuario;
+use App\Enum\Rol;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\PerfilRepository;
@@ -23,4 +30,36 @@ class PerfilController extends AbstractController
 
         return $this->json($perfil);
     }
+
+    #[Route('/crear', name: 'perfil_crear', methods: ['POST'])]
+    public function crearDto(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $datos = json_decode($request->getContent(), true);
+
+        $crearUsuarioPerfil = new CrearUsuarioPerfilDTO();
+
+        $usuario = new Usuario();
+        $usuario->setUsername($datos['username']);
+        $usuario->setPassword($datos['password']);
+        $usuario->setCorreo($datos['email']);
+        $usuario->setRol(Rol::CLIENTE);
+
+        $perfil = new Perfil();
+        $perfil->setNombre($datos['nombre']);
+        $perfil->setApellido($datos['apellidos']);
+        $perfil->setDireccion($datos['direccion']);
+        $perfil->setDni($datos['dni']);
+        $perfil->setFechaNacimiento(new \DateTime($datos['fechaNacimiento']));
+
+        $perfil->setUsuario($usuario);
+
+
+        $em->persist($usuario);
+        $em->persist($perfil);
+        $em->flush();
+
+        return $this->json(['message' => 'Usuario y Perfil creados correctamente'], Response::HTTP_CREATED);
+    }
+
+
 }
